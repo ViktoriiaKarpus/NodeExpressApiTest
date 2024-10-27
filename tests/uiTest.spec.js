234f903317e80f432cbd8b8bf2028b06ed6e4e00
ViktoriiaKarpus <103855583+ViktoriiaKarpus@users.noreply.github.com>
2024-10-27 17:54:38 +0100

    Add User form, functional test (#4)

--- tests/uiTest.spec.js
+++ tests/uiTest.spec.js
@@ -1,10 +1,10 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
-test('first test', async ({ page }) => {
+test('first test', async ({page}) => {
 
 });
 
-test('second test', async ({ page }) => {
+test('second test', async ({page}) => {
     await page.goto('http://localhost:5009/');
 
     await expect(page).toHaveURL("http://localhost:5009/");
@@ -12,14 +12,37 @@ test('second test', async ({ page }) => {
     await expect(page).toHaveTitle("Users app");
 });
 
-test('third test', async ({ page }) => {
+test('third test', async ({page}) => {
     await page.goto('http://localhost:5009/');
 
-    const headerLocator = page.getByRole('heading', { name: 'Node Express API Server App' });
-    
+    const headerLocator = page.getByRole('heading', {name: 'Node Express API Server App'});
+
     await expect(headerLocator).toBeVisible()
     //second version
     const headerCssLocator = page.locator("#appName");
-    
+
     await expect(headerCssLocator).toHaveText('Node Express API Server App');
 });
+
+test('Add User form, functional test', async ({page}) => {
+    await page.goto('http://localhost:5009/');
+
+    //const locatorAttribute = await page.getAttribute('type','text', {strict:false, timeout: 3000})
+    //console.log(locatorAttribute);
+
+    const firstNamePlaceholder = await page.getByPlaceholder("Enter first Name...", {exact: true});
+    await firstNamePlaceholder.fill("John");
+
+    const lastNameLabel = await page.getByLabel('Last Name');
+    //console.log("!!!!!!!!!",locatorLabel)
+    await lastNameLabel.fill("Do");
+
+
+    const ageId = await page.getByTestId("age");
+    await ageId.fill('34');
+
+    const addButton = await page.getByRole("button", {name:"Add", exact:true});
+    await addButton.click()
+
+    //await page.pause();
+})
